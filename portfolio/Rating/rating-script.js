60625d56a230c50c92a8549e4d61ecf1d0e795c6
Caleb <80975506+CAHernandez7@users.noreply.github.com>
2022-05-07 11:05:54 -0700

    adding some stylistic changes, fixes grammar errors

--- portfolio/Rating/rating-script.js
+++ portfolio/Rating/rating-script.js
@@ -1,4 +1,4 @@
-
+//Section for Submit Button
 //Change the background color after clicking submit
 const reviewBox = document.getElementById('review-box');
 const submitButton = document.getElementById('submit-button');
@@ -10,6 +10,17 @@ function colorChange() {
 
 //Actual event
 submitButton.addEventListener('click', colorChange);
+
+
+//Section for ul li items to turn to stars when clicked
+let listNum = [];
+const numberRatings = document.getElementById(`num-${listNum}`);
+
+function changeNumToPic() {
+    for(i = 1; i <= 10; i++) {
+        
+    }
+}
 
 
 
